32058f33015b8c34c89dc46f0dc712653155ce32
hystree0228 <hystree0228@gmail.com>
2023-11-09 16:04:47 +0900

    st docu

--- pages/Streamlit documentation.py
+++ pages/Streamlit documentation.py
@@ -4,6 +4,6 @@ st.title("Streamlit documentation")
 st.write("자주 사용하는 Streamlit documentation를 모아놓는 공간입니다.")
 
 st.write("### Install & Import")
-st.code("streamlit run first_app.py
-# Import convention
->>> import streamlit as st")
+# st.code("streamlit run first_app.py
+# # Import convention
+# >>> import streamlit as st")
